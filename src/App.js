1d44e8c1b3acb2786c9a9b1689eb03814cea4a49
ValentinDupetitpre <valentin.dupetitpre@gmail.com>
2020-12-31 13:03:46 +1300

    menu finished

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import { useState } from 'react'
 
 function App() {
   const menu = ['Apéro', 'Entrée', 'Plat', 'Dessert']
-  const [lastActive, setLastActive] = useState("")
 
   const handleMenuClick = (item) => {
-    const elem = document.getElementById(item)
-    if(lastActive) document.getElementById(lastActive).classList.remove("active")
-    elem.classList.add("active")
-    setLastActive(item)
+    const li = document.getElementById(item).parentElement
+    scrollToElement(li)
+  }
+
+  const scrollToElement = (element) => {
+    const elementCenter = element.offsetLeft + element.offsetWidth/2
+    const distanceToScrollToRight = elementCenter - window.screen.width/2
+    const step = (distanceToScrollToRight - element.parentNode.scrollLeft)/10
+    const scrolling = setInterval(() => {
+      if(distanceToScrollToRight > element.parentNode.scrollLeft){
+        element.parentNode.scrollLeft += step
+      }else if(distanceToScrollToRight < element.parentNode.scrollLeft){
+        element.parentNode.scrollLeft -= Math.abs(step)
+      }
+
+      if((distanceToScrollToRight- element.parentNode.scrollLeft <= 10 && distanceToScrollToRight > element.parentNode.scrollLeft)
+        || (element.parentNode.scrollLeft - distanceToScrollToRight <= 10 && distanceToScrollToRight < element.parentNode.scrollLeft)
+        || element.parentNode.scrollLeft === 0 || element.parentNode.scrollWidth - element.parentNode.scrollLeft - window.screen.width <=1){
+        window.clearInterval(scrolling)
+      }
+    },25)
   }
 
   return (
