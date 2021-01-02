fd0ba51b32da9dd48bac38595d5f526dcd2edc8a
ValentinDupetitpre <valentin.dupetitpre@gmail.com>
2021-01-02 21:16:37 +1300

    update menu - dropped a tag

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg'
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Switch, Route, withRouter } from 'react-router-dom'
+
+import Recipes from './components/Recipes'
 
 function App() {
   const menu = ['Apéro', 'Entrée', 'Plat', 'Dessert']
+  const [lastActive, setLastActive] = useState('Apéro')
+
+  useEffect(()=> {
+    document.getElementById("Apéro").classList.add('active')
+  }, [])
 
   const handleMenuClick = (item) => {
-    const li = document.getElementById(item).parentElement
-    scrollToElement(li)
+    const link = document.getElementById(item)
+    if(lastActive) document.getElementById(lastActive).classList.remove("active")
+    link.classList.add('active')
+    setLastActive(item)
+    scrollToElement(link.parentElement)
   }
 
   const scrollToElement = (element) => {
@@ -32,20 +42,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Fork off
+        Hākari
       </header>
       <section className="menu">
         <ul>
           { 
-            menu.map(item => <li key={item}><a href="#" key={item} onClick={() => handleMenuClick(item)} id={item}>{item}</a></li>)
+            menu.map(item => <li key={item}><p href="#" key={item} onClick={() => handleMenuClick(item)} id={item}>{item}</p></li>)
           }
         </ul>
       </section>
       <section className="content">
-          
+        <Switch>
+            <Route exact path="/" component={Recipes} />
+            {/* <Route exact path="/recipe/:recipeId" render={(props) => <Recipe {...props} />} /> */}
+        </Switch>
       </section>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/components/Recipes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Recipes = (props) => {
+    return (
+    <div>
+coucou
+    </div>
+    )
+}
+
+export default Recipes
